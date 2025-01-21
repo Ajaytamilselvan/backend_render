e013ae09e9ac028168e42302ccb3830992fc4d26
Ajaytamilselvan <122677462+Ajaytamilselvan@users.noreply.github.com>
2025-01-21 10:59:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Node.js!');
+  res.send('welcome to my first render site!');
 });
 
 app.listen(3000, () => {
